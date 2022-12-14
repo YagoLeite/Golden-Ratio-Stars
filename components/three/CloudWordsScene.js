0d1aad7537ad0ca324eb28961eefe0dd71f99a75
Yago Batista <yagobatistafis@yahoo.com.br>
2022-12-14 14:48:44 -0300

    testing with roboto

--- components/three/CloudWordsScene.js
+++ components/three/CloudWordsScene.js
@@ -24,7 +24,8 @@ const mywords = [
 function Word({ children, ...props }) {
   const color = new THREE.Color();
   const fontProps = {
-    font: "/ShadowsIntoLight-Regular.ttf",
+    // font: "/ShadowsIntoLight-Regular.ttf",
+    font: "/Roboto.woff",
     fontSize: 1.8,
     letterSpacing: -0.05,
     lineHeight: 1,
